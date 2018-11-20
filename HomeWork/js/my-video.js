b88a843141aabbc6bbb43092c8023b2bd7520c14
fivaafiva <andrey.reznikov96@gmail.com>
2018-11-20 21:57:02 +0500

    up

--- HomeWork/js/my-video.js
+++ HomeWork/js/my-video.js
@@ -1,11 +1,12 @@
-var video = document.querySelector("#video"),
-    button = document.querySelector("button");
+const video = document.querySelector("#video-main"),
+    button = document.querySelector("#button-video");
 
 button.addEventListener("click", function() {
-    video.play()
+    video.play();
     video.setAttribute("controls","controls");
 }, false);
 
 $(".video-play-btn").click(function() {
     $(".video-play-btn").addClass('video-play-btn--hide');
-})
+    $(".video-pres-desc").addClass('video-play-btn--hide');
+});
